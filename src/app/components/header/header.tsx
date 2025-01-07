6c4bd2c30ba705022b9bd965254dbd02378c6394
SamC95 <132593571+sJagstang@users.noreply.github.com>
2025-01-07 15:39:17 +0000

    feat: added header, landing hero

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./header.sass";
+
+const Header: React.FC = () => {
+    return (
+        <header className={"header"}>
+            <div className={"logo"}>
+                <span>&lt;/&gt;</span> Sam Clark
+            </div>
+            <nav className={"nav"}>
+                <ul>
+                    <li><a href={"#home"}>Home</a></li>
+                    <li><a href={"#about"}>About</a></li>
+                    <li><a href={"#qualifications"}>Qualifications</a></li>
+                    <li><a href={"#projects"}>Projects</a></li>
+                    <li><a href={"#opensource"}>Open Source</a></li>
+                    <li><a href={"#contact"}>Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,51 @@
-import Image from "next/image";
+'use client'
+
+import "../styles/landing.sass"
+import Header from "@/app/components/header/header";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoaded(true);
+        }, 100);
+        return () => clearTimeout(timer);
+    })
+
+    return (
+        <>
+            <Header/>
+
+            <div className={`hero ${isLoaded? 'loaded' : ''}`}>
+                <h1>
+                    Hi, I&#39;m Sam!
+                    <br/>
+                    <span>A UK-based Software Developer</span>
+                </h1>
+                <div className={"underline"}></div>
+                <p>
+                    I am a highly motivated, organised software engineering graduate with a passion for developing
+                    high quality software applications using front-end and back-end technologies.
+                </p>
+                <div className={"stats"}>
+                    <div>
+                        <h2>3+</h2>
+                        <p>Years Experience</p>
+                    </div>
+                    <div>
+                        <h2>5+</h2>
+                        <p>Projects Completed</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+/*
+<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -97,5 +140,4 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
-}
+ */
