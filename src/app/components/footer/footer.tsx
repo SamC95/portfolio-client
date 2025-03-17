30f2104d57c0d0a0df9a0f902b9a35f8dd878dc4
SamC95 <132593571+sJagstang@users.noreply.github.com>
2025-03-17 11:38:01 +0000

    chore: amended footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -4,7 +4,7 @@ import "./footer.sass";
 const Footer: React.FC = () => {
     return (
         <footer className="footer">
-            <p>&copy; 2025 Sam Clark. All rights reserved.</p>
+            <p>&copy; 2025 Sam Clark</p>
         </footer>
     );
 };
